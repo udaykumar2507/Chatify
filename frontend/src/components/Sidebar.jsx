5da521a1e205bce385dc0c1f5b7832b2af98253c
udaykumar2507 <uk25072004@gmail.com>
2025-04-20 14:16:04 +0530

    Default profile image updated

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -51,7 +51,7 @@ const Sidebar = () => {
           >
             <div className="relative mx-auto lg:mx-0">
               <img
-                src={user.profilePic || "/avatar.png"}
+                src={user.profilePic || "/avatar.jpg"}
                 alt={user.fullName}
                 className="size-12 object-cover rounded-full"
               />
